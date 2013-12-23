e9f076d1b0fba3006fcd2473494c1675759754a7
Matt Windsor <mbw500@york.ac.uk>
2013-12-23 22:01:59 +0000

    Some more yardsticking; refactor privilege_set.

--- lib/kankri/password_check.rb
+++ lib/kankri/password_check.rb
@@ -1,5 +1,9 @@
 module Kankri
   # A method object that represents a check on username/password pairs
+  #
+  # This is a basic check based on string comparison, failing if either
+  # username or password are empty or nil.  If used with a hashing
+  # authenticator, the hashing must be done before the password checking.
   class PasswordCheck
     # @api public
     def initialize(username, password, passwords)
@@ -9,6 +13,7 @@ module Kankri
     end
 
     # @api public
+    # @return [Boolean]  True if the password matches; false otherwise.
     def ok?
       auth_present? && user_known? && password_match?
     end
@@ -16,28 +21,33 @@ module Kankri
     private
 
     # @api private
+    # @return [Boolean]  True if the credentials are present; false otherwise.
     def auth_present?
       username_present? && password_present?
     end
 
     # @api private
+    # @return [Boolean]  True if the username is present; false otherwise.
     def username_present?
       !(@username.nil? || @username.empty?)
     end
 
     # @api private
+    # @return [Boolean]  True if the password is present; false otherwise.
     def password_present?
       !(@password.nil? || @password.empty?)
     end
 
     # @api private
-    def password_match?
-      @passwords.fetch(@username) == @password
+    # @return [Boolean]  True if the user's password is known; false otherwise.
+    def user_known?
+      @passwords.key?(@username)
     end
 
     # @api private
-    def user_known?
-      @passwords.key?(@username)
+    # @return [Boolean]  True if the password matches; false otherwise.
+    def password_match?
+      @passwords.fetch(@username) == @password
     end
   end
 end
--- lib/kankri/privilege_set.rb
+++ lib/kankri/privilege_set.rb
@@ -11,12 +11,12 @@ module Kankri
     # @example Create a privilege set with some privileges.
     #   PrivilegeSet.new({channel_set: [:get, :put]})
     def initialize(privileges)
-      @privileges = privileges
-      symbolise_privileges
+      @privileges = symbolise_privileges(privileges)
     end
 
     # Requires a certain privilege on a certain target
     # @api public
+    # @return [void]
     def require(target, privilege)
       fail(InsufficientPrivilegeError) unless has?(target, privilege)
     end
@@ -28,11 +28,11 @@ module Kankri
     #   privs.has?(:channel, :put)
     #   #=> false
     #
-    # @param target [Symbol] The handler target the privilege is for.
-    # @param privilege [Symbol] The privilege (one of :get, :put, :post or
+    # @param target [Symbol]  The handler target the privilege is for.
+    # @param privilege [Symbol]  The privilege (one of :get, :put, :post or
     #   :delete).
     #
-    # @return [Boolean] true if the privileges are sufficient; false
+    # @return [Boolean]  True if the privileges are sufficient; false
     #   otherwise.
     def has?(privilege, target)
       PrivilegeChecker.new(target, privilege, @privileges).check?
@@ -41,13 +41,17 @@ module Kankri
     private
 
     # @api private
-    def symbolise_privileges
-      @privileges = Hash[@privileges.map do |key, key_privs|
-        [key.to_sym, symbolise_privilege_list(key_privs)]
-      end]
+    # @return [Hash]  The symbolised privileges set.
+    def symbolise_privileges(privileges)
+      Hash[
+        privileges.map do |key, key_privs|
+          [key.to_sym, symbolise_privilege_list(key_privs)]
+        end
+      ]
     end
 
     # @api private
+    # @return [Object]  The symbolised privilege list.
     def symbolise_privilege_list(privlist)
       privlist.is_a?(Array) ? privlist.map(&:to_sym) : privlist.to_sym
     end
@@ -63,34 +67,40 @@ module Kankri
     end
 
     # @api public
+    # @return [Boolean]  True if the privilege is held by the privilege set
+    #   for the target; false otherwise.
     def check?
-      has_all? || has_direct?
+      target_in_privileges? && has_privilege?
     end
+
 
     private
 
     # @api private
-    # @return [Boolean] true if this privilege set has all privileges for a
-    #   target.
-    def has_all?
-      @privileges[@target] == :all
-    end
-
-    # @api private
-    # @return [Boolean] true if this privilege set explicitly has a certain
-    #   privilege for a certain target.
-    def has_direct?
-      target_in_privileges? && requisite_in_target_privileges?
-    end
-
-    # @api private
+    # @return [Boolean]  True if the target key is in the privileges set.
     def target_in_privileges?
       @privileges.key?(@target)
     end
 
     # @api private
-    def requisite_in_target_privileges?
-      @privileges[@target].include?(@requisite)
+    # @return [Boolean]  True if the privilege is held by the privilege set
+    #   for the target; false otherwise.
+    def has_privilege?
+      has_all? || has_direct?
+    end
+
+    # @api private
+    # @return [Boolean]  True if this privilege set has all privileges for a
+    #   target; false otherwise.
+    def has_all?
+      @privileges.fetch(@target) == :all
+    end
+
+    # @api private
+    # @return [Boolean]  True if this privilege set explicitly has a certain
+    #   privilege for a certain target; false otherwise.
+    def has_direct?
+      @privileges.fetch(@target).include?(@requisite)
     end
   end
 end
